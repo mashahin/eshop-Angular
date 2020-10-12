ff01905d823a1d4072a8643c199192cbba8d2705
Muhammad Shahin <mashahin2009@gmail.com>
2020-10-11 21:14:14 -0500

    modify home & shopping-cart navbar icons

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAwxo8Bt7ZeYfFLI5WrakbNGtJsskBv3wA",
-    authDomain: "oshop-f3445.firebaseapp.com",
-    databaseURL: "https://oshop-f3445.firebaseio.com",
-    projectId: "oshop-f3445",
-    storageBucket: "oshop-f3445.appspot.com",
-    messagingSenderId: "223112932947"    
+    apiKey: "AIzaSyDv3creirt5POsePe4DojrVoYldAlk87xQ",
+    authDomain: "oshop-140f7.firebaseapp.com",
+    databaseURL: "https://oshop-140f7.firebaseio.com",
+    projectId: "oshop-140f7",
+    storageBucket: "oshop-140f7.appspot.com",
+    messagingSenderId: "552009577686",
+    appId: "1:552009577686:web:9544697253f59a7a8d0577",
+    measurementId: "G-4F9Y6QCRFF"
   }
 };
